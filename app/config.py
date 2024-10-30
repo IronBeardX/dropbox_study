caa23178dd45b2b247d1156bf267227d948c53e1
Alejandro Yero Valdes <alejandroyeroval@gmail.com>
2024-10-30 11:14:55 -0400

    templating tests

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     # DocuSign API credentials
     db_api_key: str
-    
+    db_api_app_id: str
 
     class Config:
         env_file = ".env"
